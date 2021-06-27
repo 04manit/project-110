e9a14cd0d71d9c4e77f6181de8e4c91ee551c3e2
Manit Singhal <76865274+04manit@users.noreply.github.com>
2021-06-27 22:10:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+Webcam.set({
+    width: 310,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+    constraints: {FacingMode:"environment"}
+});
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src='"+data_uri+"'>";
+    });
+}
+console.log("ml5 version : ", ml5.version);
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+function modelLoaded(){
+    console.log("MobileNet loaded !");
+}
+function check(){
+    img = document.getElementById('selfie_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
